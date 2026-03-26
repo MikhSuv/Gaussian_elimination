2c05f35fba65cc962ffdcaf6e7d74c052452042b
MikhSuv <mischasuv@mail.ru>
2026-03-26 20:30:43 +0300

    Add epsilon constant

--- src/precision_mod.f90
+++ src/precision_mod.f90
@@ -3,8 +3,9 @@ module precision_mod
   implicit none
   private
 
-  public :: dp
+  public :: dp, eps
 
   integer, parameter :: dp = selected_real_kind(15,307)
+  real(dp), parameter :: eps = epsilon(1.0_dp)
 
 end module precision_mod
