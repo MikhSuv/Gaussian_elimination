2d4ff9f3cb987d4bd1643cf492e3c77b1b31e9c2
MikhSuv <mischasuv@mail.ru>
2026-03-29 11:03:48 +0300

    Fix tests output

--- test/check.f90
+++ test/check.f90
@@ -3,34 +3,45 @@ program check
   use Gaussian_elimination
 implicit none
 
-integer :: n = 3000
+integer :: n = 1000
 integer :: i, j
 real(dp), allocatable :: A(:,:), B(:), X(:)
+real(dp) :: r(6)
 
 allocate(A(n, n), B(n))
 call random_number(A)
 call random_number(B)
 
 X = solve_linear_system(A, B, "gauss")
-print *, residual(A, B, X)
+r(1)= residual(A, B, X)
 Deallocate(X)
 X = solve_linear_system(A, B, "jordan")
-print *, residual(A, B, X)
+r(2)= residual(A, B, X)
 Deallocate( X)
 X = solve_linear_system(A, B, "choice")
-print *, residual(A, B, X)
+r(3)= residual(A, B, X)
 Deallocate(A, B, X)
 
 allocate(A(n, n), B(n))
 forall (i = 1:n, j = 1:n) A(i,j) = 1.0_dp/ (real(i + j - 1, kind = dp))
 call RANDOM_NUMBER(B)
 X = solve_linear_system(A, B, "gauss")
-print *, residual(A, B, X)
+r(4)= residual(A, B, X)
 Deallocate(X)
 X = solve_linear_system(A, B, "jordan")
-print *, residual(A, B, X)
+r(5)= residual(A, B, X)
 Deallocate(X)
 X = solve_linear_system(A, B, "choice")
-print *, residual(A, B, X)
+r(6)= residual(A, B, X)
 Deallocate(A, B, X)
+
+print *, "Модули векторов невзяки"
+print *, "Случайная матрица"
+print *, "Метод Гаусса: ", r(1)
+print *, "Метод Йордана: ", r(2)
+print *, "Метод Гаусса с выбором элемента: ", r(3)
+print *, "Матрица Гильберта"
+print *, "Метод Гаусса: ", r(4)
+print *, "Метод Йордана: ", r(5)
+print *, "Метод Гаусса с выбором элемента: ", r(6)
 end program check
